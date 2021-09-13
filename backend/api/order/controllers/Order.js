22cc869c352a5bf316487eafa288c799053f9a59
Daniel Westfall <76882475+danielwestfall@users.noreply.github.com>
2021-09-13 15:50:15 -0500

    Updates
    Updated CSS and some functionality

--- backend/api/order/controllers/Order.js
+++ backend/api/order/controllers/Order.js
@@ -6,7 +6,7 @@
  */
 // note that this needs to be a "private" key from STRIPE
 const stripe = require("stripe")(
-  "sk_test_51HaLhVGgpfLkdZwmmtP4YP0CYjzHEgzhNehfIm8m7cvzKnmek68toarKoYXMsutgu4pkbegopWRG6v9NaCkq65em00Elwbb2Wi"
+  "sk_test_51JVNPVCfDt9qLvTR6cslgK0PFCmHIdtq8dgnjLxJ1H9AaNLfi8vhyTcauCRGhEYmxxCblKV6i0DeiwYhXjA5qUDN005zJ5fm7S"
 );
 module.exports = {
   /**
